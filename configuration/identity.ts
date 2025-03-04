d1590df55afb0e8eb83fbebc571d4f8f294a3c0d
anisham25 <56100314+anisham25@users.noreply.github.com>
2025-03-04 14:52:19 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Anisha Musti`;
-export const OWNER_DESCRIPTION: string = `a meditative and zen 488 student`;
+export const OWNER_DESCRIPTION: string = `a meditative and zen BUSI 488 student`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
