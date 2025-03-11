444aef07a7fe68191be96290ecaacbdc6d06f4fa
anisham25 <56100314+anisham25@users.noreply.github.com>
2025-03-11 16:06:48 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Peppa the PepTalkAI`;
+export const CHAT_HEADER: string = `Peppa, Your Mental Health Counselor`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `PepTalkAI by Anisha Musti`;
+export const FOOTER_MESSAGE: string = `PeppaAI by Anisha Musti`;
 export const CLEAR_BUTTON_TEXT: string = `New Conversation`;
-export const PAGE_TITLE: string = `PepTalkAI`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, a pep-talk AI.`;
+export const PAGE_TITLE: string = `PeppaAI`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, a mental health counselor AI.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
