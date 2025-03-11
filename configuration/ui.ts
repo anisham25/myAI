46ca37fd3c6c0a570184f54425794b1cb4ebddcc
anisham25 <56100314+anisham25@users.noreply.github.com>
2025-03-11 14:00:49 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `PepTalkAI by Anisha Musti`;
 export const CLEAR_BUTTON_TEXT: string = `New Conversation`;
 export const PAGE_TITLE: string = `PepTalkAI`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, a pep-talk AI.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
