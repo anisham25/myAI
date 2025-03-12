3cc0f9762d648632ba72d40f405d6f809a3e8ff8
Anisha <anisha.musti@gmail.com>
2025-03-12 17:16:51 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Peppa, your AI Therapist`;
+export const CHAT_HEADER: string = `Peppa, your AI Workplace Therapist`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `PeppaAI by Anisha Musti`;
 export const CLEAR_BUTTON_TEXT: string = `New Conversation`;
 export const PAGE_TITLE: string = `PeppaAI`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, a mental health counselor AI.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, a corporate mental health counselor AI.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
