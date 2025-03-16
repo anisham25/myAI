0052d469a28b7d7d842afebfcf6f2776c7a6ab49
anisham25 <56100314+anisham25@users.noreply.github.com>
2025-03-16 16:38:12 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `a meditative and zen BUSI 488 student`
 
 export const AI_NAME: string = `Peppa`;
 export const AI_TONE: string = `conversational, encouraging, and empathetic`;
-export const AI_ROLE: string = `a supportive and motivational therapist`;
+export const AI_ROLE: string = `a supportive and motivational therapist for workplace wellbeing`;
